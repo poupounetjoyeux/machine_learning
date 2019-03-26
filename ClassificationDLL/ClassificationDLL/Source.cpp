6b210cd2a76e8ddcc6abd1c2f4aed81d07c8c30d
brayquentin <brayquentin@gmail.com>
2019-03-26 14:01:32 +0100

    Updating linear regression

--- ClassificationDLL/ClassificationDLL/Source.cpp
+++ ClassificationDLL/ClassificationDLL/Source.cpp
@@ -6,26 +6,23 @@ using std::srand;
 using std::time;
 
 extern "C" {
-	__declspec(dllexport) int return5() {
-		return 5;
-	}
-
-	__declspec(dllexport) double* create_dataset(int size) {
-		double *arr = (double*)malloc(sizeof(int)*size);
+	__declspec(dllexport) double* create_dataset(int inputsDimension) {
+		double *arr = (double*)malloc(sizeof(int)*inputsDimension + 1);
 		srand(time(nullptr));
 		
-		for (int i = 0; i < size; i ++)
+		for (int i = 0; i < inputsDimension + 1; i ++)
 		{
 			arr[i] = ((double)rand() / RAND_MAX) * 2 - 1;
 		}
 		return arr;
 	}
 
-	__declspec(dllexport) void train_model(double* model, double* inputs, int dimenssion, int nbData, double* results, double step, int nbIterations) {
-		double* inputk;
+	__declspec(dllexport) void train_model(double* model, double* inputs, int inputsDimension, int nbInputs, double* expectedSigns, double learnStep, int nbIterations) {
 		for (int i = 0; i < nbIterations; i++) {
-			for (int k = 0; k < nbData; k++) {
-				inputk = inputs + k * 2; 
+			for (int k = 0; k < nbInputs; k++) {
+				double* inputk = inputs + k * 2;
+				int predictSign = predict(model, inputk, inputsDimension);
+				learn(model, inputk, inputsDimension, predictSign, expectedSigns[k], learnStep);
 			}
 		}
 	}
@@ -34,10 +31,20 @@ extern "C" {
 		return number > 0 ? 1 : -1;
 	}
 
+	void learn(double* model, double* inputk, int inputsDimension, int predictedSign, int expectedSign, double learnStep)
+	{
+		int alpha = expectedSign - predictedSign;
+		model[0] = model[0] + learnStep * alpha;
+		for (int i = 0; i < inputsDimension; i++)
+		{
+			model[i+1] = model[0] + learnStep * alpha * inputk[i];
+		}
+	}
+
 	__declspec(dllexport) double predict(double* model, double* inputk, int size) {
 		double result = model[0];
 		for (int i = 0; i < size; i++) {
-			result = model[i + 1] * inputk[i];
+			result += model[i + 1] * inputk[i];
 		}
 		return sgn(result);
 	}
