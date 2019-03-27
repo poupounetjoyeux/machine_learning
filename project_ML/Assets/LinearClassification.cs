67f7ecf270e8ebe68804f648854fe8aac06e509e
Martijn van Duijneveldt <martijnvanduijneveldt@gmail.com>
2019-03-27 12:48:56 +0100

    Updated LinearClassification to use spacebar

--- project_ML/Assets/LinearClassification.cs
+++ project_ML/Assets/LinearClassification.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
@@ -20,41 +21,56 @@ namespace Assets
         [SerializeField]
         private double _learnStep = 0.001;
 
+        private GameObject[] spheresPlan;
+        private GameObject[] spheres;
+        private List<double> inputs;
+        private IntPtr model;
+        private IEnumerable<int> expectedSigns;
+
         public double LearnStep
         {
             get => _learnStep;
             set => _learnStep = value;
         }
-        
+
         private void Start()
         {
-            var spheresPlan = GameObject.FindGameObjectsWithTag("plan");
-            var spheres = GameObject.FindGameObjectsWithTag("sphere");
+            spheresPlan = GameObject.FindGameObjectsWithTag("plan");
+            spheres = GameObject.FindGameObjectsWithTag("sphere");
 
             Debug.Log($"Sphere number : {spheres.Length}");
             Debug.Log($"PlanSphere number : {spheresPlan.Length}");
             Debug.Log("Starting to call library for a LinearClassification");
 
-            var model = ClassificationLibrary.createModel(Dimensions);
+            model = ClassificationLibrary.createModel(Dimensions);
 
-            var expectedSigns = spheres.Select(sp => sp.transform.position.y < 0 ? -1 : 1);
-            var inputs = new List<double>();
+            expectedSigns = spheres.Select(sp => sp.transform.position.y < 0 ? -1 : 1);
+            inputs = new List<double>();
             foreach (var sphere in spheres)
             {
                 inputs.Add(sphere.transform.position.x);
                 inputs.Add(sphere.transform.position.z);
             }
+        }
 
-            ClassificationLibrary.trainModelLinearClassification(model, inputs.ToArray(), Dimensions, spheres.Length, expectedSigns.ToArray(), LearnStep, NumberOfIterations);
-
-            foreach (var sphere in spheresPlan)
+        private void Update()
+        {
+            if (Input.GetKey(KeyCode.Space))
             {
-                var position = sphere.transform.position;
-                var point = new double[] {position.x, position.z};
-                var newY = ClassificationLibrary.predictClassificationModel(model, point, Dimensions);
-                sphere.transform.position = new Vector3(position.x, newY, position.z);
-            }
+                ClassificationLibrary.trainModelLinearClassification(model, inputs.ToArray(), Dimensions, spheres.Length, expectedSigns.ToArray(), LearnStep, 1);
 
+                foreach (var sphere in spheresPlan)
+                {
+                    var position = sphere.transform.position;
+                    var point = new double[] {position.x, position.z};
+                    var newY = ClassificationLibrary.predictClassificationModel(model, point, Dimensions);
+                    sphere.transform.position = new Vector3(position.x, newY, position.z);
+                }
+            }
+        }
+
+        private void OnApplicationQuit()
+        {
             ClassificationLibrary.releaseModel(model);
         }
     }
